9f947f0b9322f26cb84f6ce6f725665fc561c04e
Lucas Bertazo <saculbertazo@gmail.com>
2023-04-27 20:14:07 -0300

    all css html and some javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+/*  */
+
+document.addEventListener("click", e => {
+    const targetClasses = e.target.classList;
+    
+    if (targetClasses.contains('button')) {
+        images = e.target.
+        const correct = 1;
+        if (targetClasses.contains(`option-${correct}`)) console.log('RESPOSTA CORRETA!');
+        else console.log('RESPOSTA INCORRETA!');
+    }
+})
+
