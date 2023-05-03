55c7a2b2c875b491f425183e97a0a7c51c0d3030
Lucas Bertazo <saculbertazo@gmail.com>
2023-05-03 11:42:39 -0300

    respostas sao aleatorias, mas podem repetir

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+/* IMAGE GENERATOR */
+
+function random(min, max) {
+    return (Math.random() * (max - min) + min).toFixed();
+}
+
+function generateImage(imgBox) {
+    const max = random(1, 4);
+
+    for(let i = 0; i < max; i++) {
+        const imgElement = document.createElement("img");
+
+        imgElement.classList.add('image');
+        imgElement.src = `https://as1.ftcdn.net/v2/jpg/02/45/24/32/1000_F_245243273_1VzsxL9SynYTQfCkJ60S0EB8wMSKsmrk.jpg`
+        imgElement.alt = `Dinossauro`
+
+        imgBox.appendChild(imgElement);
+    }
+}
+
+function generateButtonResponse(imgBox) {
+    const answers = imgBox.nextElementSibling,
+          buttons = answers.querySelectorAll('.button');
+          
+    const randomButton = buttons[random(0, 2)],
+          correctResponse = imgBox.querySelectorAll(".image").length;
+
+    buttons.forEach(button => {
+        const randomNumber = random(1, 9);
+        button.innerText = randomNumber;
+        
+        randomButton.innerText = correctResponse;
+    });
+}
+
+const images = document.querySelectorAll('.images');
+
+images.forEach(imgBox => {
+    generateImage(imgBox);
+    generateButtonResponse(imgBox);
+})
+
 /* BUTTON RESPONSE */
 
 const correct = document.querySelector('.correct'),
